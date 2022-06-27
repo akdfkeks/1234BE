38b3ae0f2f9192c3e25cfa2a956eb80b2c1451a0
akdfkeks <akdfkeks@gmail.com>
2022-06-27 14:54:10 +0900

    add router for user board

--- src/user/userRouter.js
+++ src/user/userRouter.js
@@ -1,8 +1,12 @@
 "use strict";
 import { Router } from "express";
 import { getUserInfo, setUserInfo } from "./userController.js";
+import { getUserBoard, createUserBoard } from "./userController.js";
 
 export const userRouter = Router();
+
+userRouter.get("/board", getUserBoard);
+userRouter.post("/board", createUserBoard);
 
 userRouter.get("/info", getUserInfo);
 userRouter.post("/info", setUserInfo);
