845518aa465263d54ef8cbaa99db98235f3df5cc
akdfkeks <akdfkeks@gmail.com>
2022-06-27 14:54:43 +0900

    add controller for getUserBoard router

--- src/user/userController.js
+++ src/user/userController.js
@@ -1,2 +1,34 @@
-export function getUserInfo(req, res, next) {}
-export function setUserInfo(req, res, next) {}
+"use strict";
+
+import prisma from "../orm/prisma.js";
+import { logger } from "../function/logger/logger.js";
+
+export async function getUserBoard(req, res, next) {
+	const { userId } = req.body.user;
+	try {
+		const userInfo = await prisma.user.findUnique({
+			where: { userId: userId },
+			include: {
+				boardList: {
+					select: {
+						uuid: true,
+						title: true,
+						ownerId: true,
+					},
+				},
+			},
+		});
+		res.status(200).json({ success: true, message: "Succeed", data: userInfo.boardList });
+	} catch (err) {
+		logger.error(err);
+	}
+}
+export function createUserBoard(req, res, next) {
+	const { userId } = req.body;
+}
+export function getUserInfo(req, res, next) {
+	const { userId } = req.body;
+}
+export function setUserInfo(req, res, next) {
+	const { userId } = req.body;
+}
