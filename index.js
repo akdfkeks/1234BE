28fe362f64801e9ab121666e524eb295e29f2e70
akdfkeks <akdfkeks@gmail.com>
2022-07-14 09:56:38 +0900

    some change via local format

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ async function start() {
 		const PORT = process.env.PORT || 3001;
 		server.listen(PORT, "0.0.0.0", () => console.log(`Server running on port http://localhost:${PORT}`));
 	} catch (err) {
-		logger.error(err);
+		console.log(err);
+		//logger.error(err);
 	}
 }
 
